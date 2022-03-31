11feaf6065a5b31f366af8395f37d95a9dac96e3
deven7818 <deven7818@gmail.com>
2022-03-31 15:00:25 +0530

    Checks the number of records matches

--- IndianStateCencusAnlyser/src/test/java/com/indiancencusanlyser/AppTest.java
+++ IndianStateCencusAnlyser/src/test/java/com/indiancencusanlyser/AppTest.java
@@ -1,13 +1,28 @@
 package com.indiancencusanlyser;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test Cases
+ * @author Asus
+ *
+ */
 
 public class AppTest {
 	
+	StateCensusAnalysis analyser;
+	
+	@Before
+	public void initialization() {
+		analyser = new StateCensusAnalysis();
+	}
+	
 	@Test
-	public void testCensusAnalyser() {
-		StateCensusAnalysis analyser = new StateCensusAnalysis();
+	public void testRecordMatch() {
 		analyser.loadData();
+		assertEquals(true, analyser.checkData());
 	}
 }
